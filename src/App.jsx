3e94ceb9ced16aa1641d9c0bc8b55e114fd168a0
kangmoonseok87 <acuamoon1@gmail.com>
2023-10-31 20:43:58 +0900

    updata

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Country from './pages/Country';
+import Search from './pages/Search';
+import NotFound from './pages/NotFound';
 
 function App() {
  
   return (
-    <>
- 
-    </>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/search' element={<Search/>}/>
+      <Route path='/country' element={<Country/>}/>
+      <Route path='*' element={<NotFound/>}/>
+    </Routes>
   )
 }
 
