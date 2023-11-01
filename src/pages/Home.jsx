2a835a01b841428095a38073c43439624bb11a7c
kangmoonseok87 <acuamoon1@gmail.com>
2023-11-01 17:38:31 +0900

    Add async

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,20 @@
+import { useEffect, useState } from 'react';
+import { fetchCountries } from '../api/api.js';
+
 export default function Home() {
+    /* 국가데이터는 배열형태의 데이터 이므로 []를 함수에 넣어줌 */
+    const [countries, setCountries] = useState([]);
+
+    const setInitData = async() => {
+        /* fetchCountries() 함수는 async로 정의 되어 있으므로 부를때 */
+        /* await를 사용해야 함 */ 
+        const data = await fetchCountries();
+        setCountries(data);
+    }
+
+    useEffect(()=>{
+        setInitData();
+    },[])
+
     return <div>Home</div>
 }
