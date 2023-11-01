c5caab05c491317c8275eb1a2641e36d28e7240d
kangmoonseok87 <acuamoon1@gmail.com>
2023-11-01 20:04:39 +0900

    update fetch

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,3 +1,24 @@
+import { useSearchParams } from "react-router-dom";
+import { fetchSearchResults } from "../api/api";
+import { useEffect, useState } from "react";
+
 export default function Search() {
-    return <div>Search</div>
+    const [searchParams, setSearchParams] = useSearchParams()
+    const q = searchParams.get("q");
+    
+    /* 국가데이터는 배열형태의 데이터 이므로 []를 함수에 넣어줌 */
+    const [countries, setCountries] = useState([]);
+
+    const setInitData = async()=>{
+        /* fetchSearchResults() 함수는 async로 정의 되어 있으므로 부를때 */
+        /* await를 사용해야 함 */ 
+        const data = await fetchSearchResults(q);
+        setCountries(data);
+    }
+
+    useEffect(()=>{
+        setInitData();
+    },[q]);
+
+    return <div>Search {searchParams.get("q")}</div>
 }
